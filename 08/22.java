d67fb694e1a09eeb48f0ace436e7f16001eea8d2
iwannabeabird <pizzafizza@naver.com>
2026-05-06 23:09:57 +0900

    22_베스트 앨범
    Implement main method and define solution method stub.

---
+++ 08/22.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(solution(new String[]{"classic", "pop", "classic", "classic", "pop"}, new int[]{500, 600, 150 800, 2500});
+    }
+
+    // 장르
+    // 장르 내 재생 횟수
+    // 재생 횟수가 같을 경우 고유 번호 순
+
+    public static int[] solution(String[] genres, int[] plays) {
+        int[] answer = {};
+        return answer;
+    }
+}
