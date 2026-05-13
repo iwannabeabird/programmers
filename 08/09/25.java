1b73e7916d906f6008b52d3357ebabb589b71e0d
iwannabeabird <pizzafizza@naver.com>
2026-05-13 22:33:40 +0900

    25_트리 순회

---
+++ 08/09/25.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(solution(new int[]{1,2,3,4,5,6,7})));
+    }
+
+    public static String[] solution(int[] nodes) {
+        String[] result = new String[3];
+        result[0] = preorder(nodes, 0).trim(); // 마지막 공백 제거
+        result[1] = inorder(nodes, 0).trim();
+        result[2] = postorder(nodes, 0).trim();
+        return result;
+    }
+
+    private static String preorder(int[] nodes, int idx) {
+        if(idx >= nodes.length) {
+            return "";
+        }
+
+        return nodes[idx] + " " + preorder(nodes, 2 * idx + 1) +
+                preorder(nodes, 2 * idx + 2);
+    }
+
+    private static String inorder(int[] nodes, int idx) {
+        if(idx >= nodes.length) {
+            return "";
+        }
+
+        return inorder(nodes, 2 * idx + 1) + " " + nodes[idx] + inorder(nodes, 2 * idx + 2);
+    }
+
+    private static String postorder(int[] nodes, int idx) {
+        if(idx >= nodes.length) {
+            return "";
+        }
+
+        return postorder(nodes, 2 * idx + 1) + postorder(nodes, 2 * idx + 2) + nodes[idx] +  " ";
+    }
+}
